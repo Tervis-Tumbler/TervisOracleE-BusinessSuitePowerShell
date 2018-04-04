19c32a34a232f7a5c58e6080499a534dc8b38b33
ChrisMagnuson <hourglasssand@gmail.com>
2018-04-04 10:29:49 -0400

    Initial commit

---
+++ TervisOracleE-BusinessSuitePowerShell.psm1
@@ -0,0 +1,31 @@
+﻿$EBSEnvironments = [PSCustomObject]@{
+    Name = "Production"
+    Apps_ReadPasswordstateEntryID = 4624
+},
+[PSCustomObject]@{
+    Name = "Epsilon"
+    Apps_ReadPasswordstateEntryID = 2669
+},
+[PSCustomObject]@{
+    Name = "Delta"
+    Apps_ReadPasswordstateEntryID = 2852
+}
+
+function Set-TervisEBSEnvironment {
+    param (
+        [Parameter(Mandatory)]$Name
+    )
+    $Environment = $EBSEnvironments | 
+    Where-Object Name -EQ $Name
+    
+    $OracleDatabaseEntry = Get-PasswordstateOracleDatabaseEntryDetails -PasswordID $Environment.Apps_ReadPasswordstateEntryID
+    $ConnectionString = $OracleDatabaseEntry | ConvertTo-OracleConnectionString
+    Set-EBSConnectionString -ConnectionString $ConnectionString
+}
+
+Set-TervisEBSEnvironment -Name Delta
+
+function Invoke-TervisEBSResponsibilityAnalysis {
+    $EBSUserNamesAndResponsibility = Get-EBSUserNameAndResponsibility
+    $EBSUserNamesAndResponsibility | group RESPONSIBILITY_NAME | sort count
+}
