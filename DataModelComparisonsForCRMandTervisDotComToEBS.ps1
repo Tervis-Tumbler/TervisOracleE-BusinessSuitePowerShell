4a2e3778b2d901b85334e58fefda6d6de80f0c1c
ChrisMagnuson <hourglasssand@gmail.com>
2018-04-12 14:56:50 -0400

    Scratch testing

---
+++ DataModelComparisonsForCRMandTervisDotComToEBS.ps1
@@ -0,0 +1,80 @@
+﻿$EBSTCARecordsNeededForSalesOrderAcknowledgement = [PSCustomObject]@{
+    Type = "Organization"
+    Account = [PSCustomObject]@{
+        Contact = @(
+            [PSCustomObject]@{
+                Email = @(
+                    [PSCustomObject]@{
+                        Primary = $true
+                        Address = "email@domain.com"
+                    }
+                )
+                Role = @(
+                    [PSCustomObject]@{
+                        Name = "Acknowledgements"
+                    }
+                )
+            }
+        )
+    }
+}
+
+$EBSTCARecordsNeededForShipmentEmail = [PSCustomObject]@{
+    Type = "Organization"
+    Account = [PSCustomObject]@{
+        Site = @(
+            [PSCustomObject]@{
+                Purpose = "Ship To"
+                Contact = @(
+                    [PSCustomObject]@{
+                        Email = @(
+                            [PSCustomObject]@{
+                                Primary = $true
+                                Address = "email@domain.com"
+                            }
+                        )
+                    }
+                )
+            }
+        )
+    }
+}
+
+$EBSTCARecordsFromTervisDotCom = [PSCustomObject]@{
+    Type = "Organization"
+    
+}
+
+$EBSTCARecordsCustomerFromSalesOrderForm = [PSCustomObject]@{
+    Type = "Organization"
+    #Account = [
+}
+
+$TervisDotComLogicalDataModelForOrder = [PCustomObject]@{
+    SFCCUniqueAccountID = "1234"
+    AccountEmailAddress = "email@domain.com"
+    ContactEmail = "email@domain.com"
+    ShippingAddress = [PSCustomObject]@{
+        FirstName = ""
+        LastName = ""
+        CompanyName = ""
+        Address1 = ""
+        Address2 = ""
+        City = ""
+        State = ""
+        Zip = ""
+        PhoneNumber = ""
+    }
+    BillingAddressSameAsShipping = $False
+    BillingAddress = [PSCustomObject]@{
+        FirstName = ""
+        LastName = ""
+        CompanyName = ""
+        Address1 = ""
+        Address2 = ""
+        City = ""
+        State = ""
+        Zip = ""
+        PhoneNumber = ""
+    }
+}
---
+++ Untitled1.ps1
@@ -0,0 +1,68 @@
+﻿ipmo -Force OracleE-BusinessSuitePowerShell, TervisOracleE-BusinessSuitePowerShell, InvokeSQL
+$Relationships = Get-EBSTradingCommunityArchitectureContactPoint -EmailAddress $EmailAddress |
+ForEach-Object -Process {
+    [PSCustomObject]@{
+        Party_ID = $_.OWNER_TABLE_ID
+    }
+} |
+Get-EBSTradingCommunityArchitectureRelationship
+
+$ContactParty = $Relationships | 
+where RELATIONSHIP_TYPE -EQ "Contact" |
+ForEach-Object -Process {
+    [PSCustomObject]@{
+        Party_ID = $_.subject_id
+    }
+} |
+Get-EBSTradingCommunityArchitectureParty
+
+$CustomerAccount = $Relationships | 
+where RELATIONSHIP_TYPE -EQ "Contact" |
+ForEach-Object -Process {
+    [PSCustomObject]@{
+        Party_ID = $_.object_id
+    }
+} |
+Get-EBSTradingCommunityArchitectureCustomerAccount
+
+$OrganiztaionParty = $CustomerAccount |
+Get-EBSTradingCommunityArchitectureParty
+
+
+ipmo -Force OracleE-BusinessSuitePowerShell, TervisOracleE-BusinessSuitePowerShell, InvokeSQL
+
+$Organization = Get-EBSOrganiztaionFromEmail -EmailAddress $EmailAddress | select -First 1
+
+$Account = Get-EBSTradingCommunityArchitectureCustomerAccount -Party_ID $Organization.PARTY_ID
+$AccountSites = $Account | Get-EBSTradingCommunityArchitectureCustomerAccountSite
+$Relationships = Get-EBSTradingCommunityArchitectureRelationship -object_id $Organization.PARTY_ID
+$AccountParties = $AccountRelationships | Get-EBSTradingCommunityArchitectureParty
+$AccountRelationshipSubjectParties = $AccountRelationships |
+ForEach-Object {
+    Get-EBSTradingCommunityArchitectureParty -Party_ID $_.subject_id
+}
+$Sites = $Account | Get-EBSTradingCommunityArchitecturePartySite
+
+$Contacts = $Relationships |
+% {
+  Get-EBSTradingCommunityArchitectureOrganiztaionContact -party_relationship_id $_.RELATIONSHIP_ID
+}
+
+
+"Organization"
+$Organization | ft *
+"Account"
+$Account | ft *
+"AccountSites"
+$AccountSites | ft *
+"Relationships"
+$Relationships | ft *
+"AccountParties"
+$AccountParties | ft *
+"AccountRelationshipSubjectParties"
+$AccountRelationshipSubjectParties | ft *
+"Sites"
+$Sites | ft *
+"Contacts"
+$Contacts | ft *
+
