7f07627c2f6fc1221a6a7f9c675d3a088a45cf34
ChrisMagnuson <hourglasssand@gmail.com>
2018-06-01 16:17:45 -0400

    Update phone number analysis

--- PhoneNumberAnalysis.ps1
+++ PhoneNumberAnalysis.ps1
@@ -7,9 +7,28 @@ SELECT
     PHONE_NUMBER,
     RAW_PHONE_NUMBER
 FROM hz_contact_points
+WHERE CONTACT_POINT_TYPE = 'PHONE'
 "@
 
 $Results = Invoke-OracleSQL -ConnectionString $EBSEnvironmentConfiguration.DatabaseConnectionString -SQLCommand $SQLCommand
+
+$Results.Count
+
+$ResultsSubset = $Results | Select-Object -First 10000 -Skip 10000
+$ResultsSubset.Count
+
+$ResultsSubset | group PHONE_AREA_CODE
+$Results  | group PHONE_AREA_CODE | sort count
+
+[System.GC]::Collect()
+$IndexesOfRandomSample = 1..16547 | %{ Get-Random -Maximum $Results.Count}
+
+1..100 | %{ Get-Random -Maximum $Results.Count} | %{$Results[$_] }
+
+$ResultsSubset = $Results[$IndexesOfRandomSample]
+
+$ResultsSubset | group PHONE_AREA_CODE | sort count
+
 $Results | Export-clixml -Path .\results.xml
 $Results | convertto-json | out-file -FilePath results.json -Encoding ascii
 $Results[10000] | convertto-json
